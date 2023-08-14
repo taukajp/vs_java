50890d057f5f092ce3ebc05c7ce89841feeede52
Taukajp <taukajp@icloud.com>
2023-08-14 14:57:49 +0900

    VS Code対応

---
+++ src/test/java/com/example/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    public AppTest() {
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+}
